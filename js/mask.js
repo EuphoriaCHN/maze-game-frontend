560e6418e1e609a5b7ad414dbe6007781768bef8
EuphoriaCHN <879969355@qq.com>
2019-12-15 01:19:16 +0800

    Mask layer animation and logic

--- js/mask.js
+++ js/mask.js
@@ -3,6 +3,40 @@
 (function (window, undefined) {
     // 遮罩层逻辑
     jQuery(function () {
+        $('#start').on('click', function (ev) {
+            let mask = $('#mask');
 
+            if (typeof mask.get(0).timerInterval !== 'undefined') {
+                window.clearInterval(mask.get(0).timerInterval);
+            }
+
+            mask.get(0).timerInterval = window.setInterval(function () {
+                let offsetLeft = mask.position().left;
+
+                let offsetTop = mask.position().top;
+                let width = mask.width();
+
+                let height = mask.height();
+
+                if (width >= 1100) {
+                    window.clearInterval(mask.get(0).timerInterval);
+                    mask.hide().css('z-index', 0);
+                    $('#main').fadeIn(400).css('display', 'flex');
+                    $('#mainPage').hide();
+                }
+
+                let change = Math.floor(((1100 - width)) / 10);
+                if (change <= 10) {
+                    change = 10;
+                }
+
+                mask.css({
+                    width: width + change + 'px',
+                    height: height + change + 'px',
+                    left: offsetLeft - change / 2 + 'px',
+                    top: offsetTop - change / 2 + 'px'
+                });
+            }, 30);
+        });
     });
 })(window, undefined);
