4040b2251bfbb7a179d607e2f6b995b7018ae1bb
EuphoriaCHN <879969355@qq.com>
2020-01-08 01:05:01 +0800

    实现了结果的动画效果

--- js/drawLines.js
+++ js/drawLines.js
@@ -2,7 +2,28 @@
 
 ((window, undefined) => {
     jQuery(function () {
+        /**
+         * 清空路径动画
+         */
+        window.clearAnswersAnimation = function () {
+            // 格式化旧路径
+            if (typeof window.answerLines === 'undefined') {
+                window.answerLines = [];
+            } else {
+                while (window.answerLines.length !== 0) {
+                    let temp = answerLines.pop();
+                    window.clearInterval(temp.displayTimer);
+                    temp.stop().remove(); // 清空旧路径
+                }
+            }
+        };
+        /**
+         * 绘制结果路线
+         * @param data 结果步伐状态列表
+         */
         window.drawAnswerLines = function (data) {
+            window.clearAnswersAnimation();
+
             let mazeBody = $('#mazeBody');
             let eachRows = mazeBody.children('.row');
 
@@ -12,22 +33,58 @@
             // 控制迷宫的每一行元素
             let curRow = start[0], curCol = start[1];
             // 步数游标，查询当前走向
-            let i = 0;
+            let index = 0;
 
             var timer = window.setInterval(function () {
-                if (i === steps.length - 1) {
+                if (index === steps.length - 1) {
                     // 走到了终点前一个就退出，以防覆盖终点
                     window.clearInterval(timer);
                     return;
                 }
-                curRow += window.dx[parseInt(steps[i]) - 1];
-                curCol += window.dy[parseInt(steps[i]) - 1];
-                i += 1;
+                curRow += window.dx[parseInt(steps[index]) - 1];
+                curCol += window.dy[parseInt(steps[index]) - 1];
+                index += 1;
 
                 let now = $(eachRows.get(curRow).children[curCol]);
+                let offset = 16; // 把这个元素缩小（要不然图片太大）
 
-                now.addClass('line');
-            }, 50);
+                // 直接将这个路径节点添加到当前迷宫区块内作为一个子元素存在
+                let newStep = $("<div></div>");
+                newStep.addClass('line-block').css({
+                    width: Math.max(...[1, window.blockWidth - offset]) + 'px',
+                    height: Math.max(...[1, window.blockHeight - offset]) + 'px',
+                    top: offset / 2 + 'px', // 设置了 position: relative
+                    left: offset / 2 + 'px'
+                });
+
+                let decorationString = [window.dx[parseInt(steps[index]) - 1], window.dy[parseInt(steps[index]) - 1]].toString();
+                if (decorationString === [-1, 0].toString()) {
+                    // TOP
+                    // 向上则不用旋转，背景图片本身就是向上的
+                } else if (decorationString === [1, 0].toString()) {
+                    // DOWN
+                    newStep.css('transform', 'rotate(180deg)');
+                } else if (decorationString === [0, -1].toString()) {
+                    // LEFT
+                    newStep.css('transform', 'rotate(-90deg)');
+                } else {
+                    // RIGHT
+                    newStep.css('transform', 'rotate(90deg)');
+                }
+
+                // 设置动画
+                newStep.displayTimer = window.setInterval(function () {
+                    newStep.fadeToggle(300, function () {
+                        newStep.fadeToggle(300);
+                    });
+                }, 1000);
+
+                // 添加至迷宫区块中作为子元素
+                now.append(newStep);
+
+                // 将当前步伐装载至队列
+                window.answerLines.push(newStep);
+            }, 120);
         };
     });
 })(window, undefined);
--- js/submit.js
+++ js/submit.js
@@ -2,7 +2,12 @@
 
 ((window, undefined) => {
     jQuery(() => {
+        /**
+         * 当点击了 submit 按钮时调用
+         * @returns {boolean} 如果为 false 代表提交失败
+         */
         window.submitMaze = function () {
+            // 如果未设置起点或者终点
             if (typeof window.startPoint === 'undefined') {
                 window.drawTipsFloatBar('你还没有设置起点哦', 'error-tips');
                 return false;
@@ -14,23 +19,31 @@
             let mazeBody = $('#mazeBody');
             let eachRows = mazeBody.children('.row');
 
+            // 记录数据，为广搜做准备
             let maze = [];
             let start = [];
             let end = [];
 
+            // 遍历迷宫的每一行
             for (let i = 0; i < eachRows.length; i++) {
+                // 获得当前行
                 let row = eachRows.get(i);
                 let eachCols = $(row).children();
                 let rows = [];
                 for (let j = 0; j < eachCols.length; j++) {
+                    // 获得当前区块
                     let block = $(eachCols.get(j));
                     if (block.hasClass('wall')) {
-                        rows.push(1);
+                        // 如果是墙，则令其为 -1
+                        rows.push(-1);
                     } else {
+                        // 反之为 0
                         rows.push(0);
                         if (block.hasClass('start')) {
+                            // 如果当前元素是起点
                             start = [i, j];
                         } else if (block.hasClass('end')) {
+                            // 如果当前元素是终点
                             end = [i, j];
                         }
                     }
@@ -39,6 +52,7 @@
             }
 
             new Promise((resolve, reject) => {
+                // 发送请求
                 jQuery.ajax({
                     // url: 'http://maze.wqh4u.cn/api/gettingMaze',
                     url: 'http://127.0.0.1:8000/api/gettingMaze',
@@ -58,8 +72,8 @@
                 });
             })
                 .then(function (res) {
-                    console.log(res);
                     if (res.length === 0) {
+                        // 如果返回了一个空列表则代表没有去终点的方法
                         window.drawTipsFloatBar('无路可走！', 'error-tips');
                     } else {
                         window.drawAnswerLines({
@@ -68,6 +82,7 @@
                             end: end
                         });
                     }
+                    return true;
                 }, function (e) {
                     if (e.statusText) {
                         window.drawTipsFloatBar(e.statusText, 'error-tips');
@@ -75,6 +90,7 @@
                         window.drawTipsFloatBar('服务器错误，请稍后再试哦~', 'error-tips');
                     }
                     console.error(e);
+                    return false;
                 });
         };
 
