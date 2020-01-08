c8fcaf6e35c1ecbc176711a21c12ff7ae120dd66
EuphoriaCHN <879969355@qq.com>
2020-01-08 16:30:53 +0800

    优化了脚印方向

--- js/drawLines.js
+++ js/drawLines.js
@@ -2,6 +2,23 @@
 
 ((window, undefined) => {
     jQuery(function () {
+        // 定义脚步方向
+        let decorations = {
+            'UP': [-1, 0].toString(),
+            'DOWN': [1, 0].toString(),
+            'LEFT': [0, -1].toString(),
+            'RIGHT': [0, 1].toString()
+        };
+
+        /**
+         * 旋转脚印
+         * @param step 当前脚印 jQuery 对象
+         * @param rotateDeg 旋转角度
+         */
+        let setStepDecoration = (step, rotateDeg) => {
+            step.css('transform', `rotate(${rotateDeg}deg)`);
+        };
+
         /**
          * 清空路径动画
          */
@@ -36,6 +53,8 @@
             let curRow = start[0], curCol = start[1];
             // 步数游标，查询当前走向
             let index = 0;
+            // 获得上一步的脚印方向
+            let before = undefined;
 
             window.createLineTimer = window.setInterval(function () {
                 if (index === steps.length - 1) {
@@ -48,7 +67,7 @@
                 index += 1;
 
                 let now = $(eachRows.get(curRow).children[curCol]);
-                let offset = 16; // 把这个元素缩小（要不然图片太大）
+                let offset = 24; // 把这个元素缩小（要不然图片太大）
 
                 // 直接将这个路径节点添加到当前迷宫区块内作为一个子元素存在
                 let newStep = $("<div></div>");
@@ -60,19 +79,86 @@
                 });
 
                 let decorationString = [window.dx[parseInt(steps[index]) - 1], window.dy[parseInt(steps[index]) - 1]].toString();
-                if (decorationString === [-1, 0].toString()) {
+                if (decorationString === decorations['UP']) {
                     // TOP
-                    // 向上则不用旋转，背景图片本身就是向上的
-                } else if (decorationString === [1, 0].toString()) {
+                    console.log("TOP");
+                    if (typeof before !== 'undefined') {
+                        if (before === decorations['LEFT']) {
+                            // 上一步是向左走的，现在往上走，那么脚印因该朝着左上
+                            setStepDecoration(newStep, -45);
+                        } else if (before !== decorations['UP']) {
+                            // 因为这一步是向上走的，那么上一步不可能是向下走，只可能是向右
+                            // 那么这一步应该往右上
+                            setStepDecoration(newStep, 45);
+                        }
+                    }
+                    // 反之要不然这是第一步，图片本身就是向上的，不用旋转
+                    // 要么上一步也是向上的，图片本身就是向上的，不用旋转
+                } else if (decorationString === decorations['DOWN']) {
                     // DOWN
-                    newStep.css('transform', 'rotate(180deg)');
-                } else if (decorationString === [0, -1].toString()) {
+                    console.log('DOWN');
+                    if (typeof before !== 'undefined') {
+                        if (before === decorations['DOWN']) {
+                            // 上一步也是往下走的，那么这一步不变
+                            setStepDecoration(newStep, 180);
+                        } else {
+                            if (before === decorations['LEFT']) {
+                                // 上一步是向左走的，现在往下走，那么脚印因该朝着左下
+                                setStepDecoration(newStep, 225);
+                            } else {
+                                // 因为这一步是向下走的，那么上一步不可能是向下走，只可能是向右
+                                // 那么这一步应该往右下
+                                setStepDecoration(newStep, 135);
+                            }
+                        }
+                    } else {
+                        // 这是第一步，则直接设置
+                        setStepDecoration(newStep, 180);
+                    }
+                } else if (decorationString === decorations['LEFT']) {
                     // LEFT
-                    newStep.css('transform', 'rotate(-90deg)');
+                    console.log('LEFT');
+                    if (typeof before !== 'undefined') {
+                        if (before === decorations['LEFT']) {
+                            // 上一步也是往左走的，那么这一步不变
+                            setStepDecoration(newStep, -90);
+                        } else {
+                            if (before === decorations['UP']) {
+                                // 上一步是向上走的，现在往左走，那么脚印因该朝着左上
+                                setStepDecoration(newStep, -45);
+                            } else {
+                                // 因为这一步是向左走的，那么上一步不可能是向右走，只可能是向下
+                                // 那么这一步应该往左下
+                                setStepDecoration(newStep, 225);
+                            }
+                        }
+                    } else {
+                        // 这是第一步，则直接设置
+                        setStepDecoration(newStep, -90);
+                    }
                 } else {
                     // RIGHT
-                    newStep.css('transform', 'rotate(90deg)');
+                    console.log('RIGHT');
+                    if (typeof before !== 'undefined') {
+                        if (before === decorations['RIGHT']) {
+                            // 上一步也是往右走的，那么这一步不变
+                            setStepDecoration(newStep, 90);
+                        } else {
+                            if (before === decorations['UP']) {
+                                // 上一步是向上走的，现在往左走，那么脚印因该朝着右上
+                                setStepDecoration(newStep, 45);
+                            } else {
+                                // 因为这一步是向左走的，那么上一步不可能是向左走，只可能是向下
+                                // 那么这一步应该往右下
+                                setStepDecoration(newStep, 135);
+                            }
+                        }
+                    } else {
+                        // 这是第一步，则直接设置
+                        setStepDecoration(newStep, 90);
+                    }
                 }
+                before = decorationString;
 
                 // 设置动画
                 newStep.displayTimer = window.setInterval(function () {
